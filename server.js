9bfa263cd5715c383df5accf86ba56b2be89c237
Ben Galloway <bdgalloway85@gmail.com>
2020-12-12 23:36:09 -0600

    front end css added and js added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static("public"));
 
 const exphbs = require('express-handlebars');
 
