856676606270ecfb5ad88e685029fff9c4bd9b74
Ben Galloway <bdgalloway85@gmail.com>
2020-12-13 18:35:29 -0600

    ajax post

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,1 +1,23 @@
-//alert("hey");
+$(document).ready(function () {
+	$("#submit").on("click", function (e) {
+		e.preventDefault();
+		console.log(e);
+		const burgerName = $("#burger").val();
+		console.log(burgerName);
+		submitData(burgerName);
+	});
+	function submitData(burger) {
+		var settings = {
+			url: "http://localhost:8080/",
+			method: "POST",
+			timeout: 0,
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: JSON.stringify({ "burger_name": burger }),
+		};
+		$.ajax(settings).done(function (response) {
+			console.log(response);
+		});
+	}
+});
