4f4843f630f09af68050fb1b784f5db8724b6e42
Ben Galloway <bdgalloway85@gmail.com>
2020-12-13 19:21:19 -0600

    devour buttons active

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -4,23 +4,41 @@ $(document).ready(function () {
 		//console.log(e);
 		const burgerName = $("#burger").val();
 		//console.log(burgerName);
-        submitData(burgerName);
-        $("#burger").val("");
-        location.reload();
-
+		submitData(burgerName);
+		$("#burger").val("");
+		location.reload();
 	});
 	function submitData(burger) {
-		var settings = {
+		const settings = {
 			url: "http://localhost:8080/",
 			method: "POST",
 			timeout: 0,
 			headers: {
 				"Content-Type": "application/json",
 			},
-			data: JSON.stringify({ "burger_name": burger }),
+			data: JSON.stringify({ burger_name: burger }),
 		};
 		$.ajax(settings).done(function (response) {
 			console.log(response);
 		});
 	}
+
+	$(".devour").on("click", function (e) {
+		console.log(`devour clicked`);
+        console.log(e.currentTarget.id);
+        const settings = {
+			url: "http://localhost:8080/",
+			method: "PATCH",
+			timeout: 0,
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: JSON.stringify({ id: e.currentTarget.id }),
+		};
+		$.ajax(settings).done(function (response) {
+			console.log(response);
+		});
+        location.reload();
+
+	});
 });
